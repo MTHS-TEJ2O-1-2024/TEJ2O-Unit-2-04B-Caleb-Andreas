c8490d36b92f0a72d56c1a9ad0ff898e7554e1e1
Caleb-Andreas <caleb.andreas@mths.ca>
2024-09-19 11:22:19 -0400

    Made code that shows the temperature

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Caleb Andreas
+ * Created on: Sep 2024
+ * This program shows the temperature.
 */
 
-basic.showString('Hello, World!')
+// happy face at start
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// variable for temperature
+let findingTemperature = input.temperature()
+
+// display temperature when the a button is pressed
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showString('The temperature is: ')
+    basic.showNumber(findingTemperature)
+    basic.showString('C.')
+    basic.showIcon(IconNames.Happy)
+})
