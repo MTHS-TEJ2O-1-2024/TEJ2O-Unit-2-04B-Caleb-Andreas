517e459aeaeed998e1bca6de39792fc1ce94b62b
Caleb-Andreas <caleb.andreas@mths.ca>
2024-09-20 15:18:40 +0000

    Made code that shows temperature

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,24 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Caleb Andreas
+Created on: Sep 2024
+This program shows the temperature.
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+# Variable for temperature
+finding_Temperature = input.temperature()
+
+# Happy face at start.
+display.clear()
+display.show(Image.HAPPY)
+
+# Display temperature when the A button is pressed.
+while True:
+    if button_a.is_pressed():
+        display.clear()
+        display.scroll("The temperature is: ")
+        display.scroll(str(finding_Temperature))
+        display.scroll("C.")
+        display.show(Image.HAPPY)
